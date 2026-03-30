d7b9e0a262257b76785341d812216ccaa0a3e6c5
KeshavRaj0019 <dskeshav1946@gmail.com>
2026-03-30 21:45:03 +0530

    Added CodeBase and Resources

---
+++ CodeBase/CAKE_Control/CAKE_Control.ino
@@ -0,0 +1,12 @@
+//This directory only contains the code for the overall computational and control unit of the bot.
+#include "Config.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ CodeBase/CAKE_Control/Config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//ENCODER Data Receiver Pins
+const int I2C_SDA = 20;
+const int I2C_SCL = 21;
+
+#endif
